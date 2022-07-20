ea8b6416c170c6ba9b9098aa82c3410393b9979f
YOSS THE DEV <yossthedev@gmail.com>
2022-07-20 19:14:55 -0400

    -

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express, { Express, Request, Response } from 'express';
-import getWallpaper from './services/scraper';
+import scraper from './services/scraper';
+import url from 'url'
 
 const app: Express = express();
 const port = process.env.PORT || 4000;
@@ -17,9 +18,28 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.get('/get', async (req: Request, res: Response) => {
-  res.send(await getWallpaper('https://mobile.alphacoders.com/by-category/32?page=2'));
-});
+/*app.get('/get', async (req: Request, res: Response) => {
+  res.send(await scraper.Scraper.getWallpaper('https://mobile.alphacoders.com/by-category/32?page=2'));
+});*/
+
+
+//Ger Random Wallpaper parameters => category , resolution
+app.get("/api/getWallpaper",async (req, res) => {
+
+  //Get Url Parameter
+  const category = url.parse(req.url,true).query["category"];
+  const resolution = url.parse(req.url,true).query["resolution"];
+
+
+  //scraper.Scraper.getRandomWallpaper(category,resolution)
+
+  //console.log('http://'  + urls);
+
+  res.send(`The category is ${category} and the resolution is ${resolution} => ${scraper.Categories.Animal}`);
+
+  //Respond
+  //res.json({'urls': getWallpaperImg('https://' + urls)});
+})
 
 
 app.listen(port, () => {
