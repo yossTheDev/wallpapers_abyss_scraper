40d5ef65c043d1796b6bf0d6405084ea3762a846
YOSS <98903135+yossTheDev@users.noreply.github.com>
2022-07-30 10:16:13 -0400

    Update scraper.ts

--- services/scraper.ts
+++ services/scraper.ts
@@ -89,6 +89,8 @@ export async function getRandomWallpaper(resolution : Resolution, categories: st
         //Get All wallpapers in the defined category page
         let wallpapers = await getDesktopWallpaper(makeLink(resolution,categories) + `&page=${randomNumber(0,100)}`)
 
+        console.log(wallpapers);
+
         //Get Random Wallpaper
         wallpaper = wallpapers[randomNumber(0,wallpapers.length)];
         
